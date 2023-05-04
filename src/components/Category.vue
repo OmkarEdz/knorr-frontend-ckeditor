7a885299a96d908bdf70b43b717362b1bb1f30bf
OmkarEdz <omkar.t@edreamz.in>
2023-05-04 15:56:59 +0530

    Updated code for shared seats

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -113,7 +113,6 @@ export default {
 
     fetchCategories() {
       var _this = this;
-      showLoadingCircle(true);
       this.$axios
         .get("api/category/root")
         .then(function (response) {
