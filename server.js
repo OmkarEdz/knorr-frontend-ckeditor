31cf497ee8711113c0bf9cb37de4e2d5807d873e
Shreejith <nairshreejith1999@gmail.com>
2023-05-26 00:44:33 +0530

    image update

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('*/js',express.static(path.join(__dirname , "/target/dist/",'/static/js'
   etag: true,
   maxAge: '1d'
 }));
-app.use('*/img',express.static(path.join(__dirname , "/public/static/"),{
+app.use('*/img',express.static(path.join(__dirname , "/public/static/img"),{
   etag: true,
   maxAge: '1d'
 }));
